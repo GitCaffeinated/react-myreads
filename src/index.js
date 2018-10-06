944a8b12a2300f5be2359572d76176aabee5a687
JennaKoslowski <jenna.koslowski@yahoo.com>
2018-10-06 06:23:13 -0500

    add BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<BrowserRouter> 
+	<App /> 
+	</BrowserRouter>, document.getElementById('root'))
