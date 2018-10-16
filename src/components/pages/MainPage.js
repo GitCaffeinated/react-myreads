499cb8ee27126a4f605bd1b6a198661603e65941
JennaKoslowski <jenna.koslowski@yahoo.com>
2018-10-15 21:50:04 -0500

    add componentDidMount

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -6,9 +6,21 @@ import * as BooksAPI from '../../BooksAPI'
 
 
 class MainPage extends React.Component {
+    //used lesson 4 as template
+  state = {
+   books: []
+}
+
+  componentDidMount(){
+    BooksAPI.getAll().then(BookList=>(
+      this.setState({books: BookList})
+      ))
+  }
+
+
+
 	render() {
 		return (
-      <div className="app">
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -21,7 +33,7 @@ class MainPage extends React.Component {
               <NavLink to='/search' className="to-search-bar">Add a book</NavLink>
             </div>
           </div>
-      </div>
+   
       );}
     }
 
