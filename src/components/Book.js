4b537b7fc0acc34c005a19877c3c325007e0d7cf
JennaKoslowski <jenna.koslowski@yahoo.com>
2018-10-17 20:54:04 -0500

    books not displaying pt 2

--- src/components/Book.js
+++ src/components/Book.js
@@ -10,7 +10,8 @@ class Book extends React.Component {
 
         return (
           <div className="book">
-            <img className="book-cover">{this.props.backgroundImage}</img>
+            <div className="book-top currentlyReading">
+              <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
               <div className="book-shelf-changer">
                 <select>
                   <option value="move" disabled>Move to...</option>
@@ -23,6 +24,7 @@ class Book extends React.Component {
               </div>
             <div className="book-title">Ender's Game</div>
             <div className="book-authors">Orson Scott Card</div>
+          </div>
           </div>
 
         ) } }
--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -3,15 +3,17 @@ import React from 'react'
 
 class Shelf extends React.Component {
 
+componentDidMount(){
+  console.log(this);
+}
+
   render(){
     return (
       <div className="bookshelf">
         <h2 className="bookshelf-title">{this.props.title}</h2>
         <div className="bookshelf-books">
         <ol className= "books-grid">
-         {
-          this.props.books.map((book, key) => <Book book={book} key={key} />)
-        }
+          <Book chosenBook={this.state.books.filter((book) => book.shelf={book.shelf}) /> ) }
         </ol>
         </div>
     </div>
