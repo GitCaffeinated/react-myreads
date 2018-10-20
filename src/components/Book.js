6360f9475ccb34f63f1f19bc0e8963d581be8384
JennaKoslowski <jenna.koslowski@yahoo.com>
2018-10-20 09:44:31 -0500

    books and images show up

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Book extends Component {
+class Book extends React.Component {
 
   render() {
 
         return (
           <div className="book">
             <div className="book-top">
-              <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("${book.imageLinks.thumbnail}"")' }}></div>
+              <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url("${this.props.book.imageLinks.thumbnail}")` }}></div>
               <div className="book-shelf-changer">
                 <select>
                   <option value="move" disabled>Move to...</option>
@@ -18,9 +18,9 @@ class Book extends Component {
                   <option value="none">None</option>
                 </select>
               </div>
+              </div>
             <div className="book-title">{this.props.book.title}</div>
-            <div className="book-authors">{this.props.book.author}</div>
-          </div>
+            <div className="book-authors">{this.props.book.authors}</div>
           </div>
 
         ) } }
