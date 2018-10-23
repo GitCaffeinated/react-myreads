31fc072fae2bca3bc0b0578be58cc6bfdf7a0593
JennaKoslowski <jenna.koslowski@yahoo.com>
2018-10-22 20:30:16 -0500

    begin setState and shelf change

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,13 +4,24 @@ class Book extends Component {
 
   render() {
 
+      state={
+         value: ''
+       };
+       this.update=this.update.bind(this.state.book);
+       this.handleSubmit= this.handleSubmit.bind(this);
+   
+
+   update(e) {
+    this.setState({value: e.target.value});
+   }
+
         return (
           <div className="book">
             <div className="book-top">
               <span>{this.props.book.description}</span>
               <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url("${this.props.book.imageLinks.thumbnail}")` }}></div>
               <div className="book-shelf-changer">
-                <select>
+                <select value={this.state.value} onChange={this.update}>
                   <option value="move" disabled>Move to...</option>
                   <option value="currentlyReading">Currently Reading</option>
                   <option value="wantToRead">Want to Read</option>
